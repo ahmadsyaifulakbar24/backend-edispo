389c53f89ed7a5c8cd0380bbaa1bf1b7887f813a
ahmadsyaifulakbar24 <ipulbelcram@gmail.com>
2022-03-09 18:30:59 +0700

    add timestaps in agenda

--- app/Http/Resources/Agenda/AgendaResource.php
+++ app/Http/Resources/Agenda/AgendaResource.php
@@ -26,6 +26,8 @@ class AgendaResource extends JsonResource
             'date' => $this->date,
             'location' => $this->location,
             'description' => $this->description,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
         ];
     }
 }
