fb9b673b731afd0ab4af26c5ee2717660a91d131
edysekolahmu <edy.supardi@sekolahmu.co.id>
2022-09-19 14:08:14 +0700

    revoke fcm token when logout

--- app/Http/Controllers/API/Auth/LogoutController.php
+++ app/Http/Controllers/API/Auth/LogoutController.php
@@ -5,12 +5,16 @@ namespace App\Http\Controllers\API\Auth;
 use App\Helpers\ResponseFormatter;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\FcmToken;
 
 class LogoutController extends Controller
 {
     public function __invoke(Request $request)
     {
-        $token = $request->user()->token()->revoke();
-        return ResponseFormatter::success($token, 'Token Revoked');
+        $token = $request->user()->token();
+        $oauthId = $token->id;
+        FcmToken::where("oauth_id", $oauthId)->update(["status" => '0']);
+        $revoke = $token->revoke();
+        return ResponseFormatter::success($revoke, 'Token Revoked');
     }
 }
